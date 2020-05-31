1df0ecd9c6f12bd4106977743c16c2766627cd4c
vishakahegde <hegde.vishaka@gmail.com>
2020-05-31 18:45:20 +0200

    Create dummy reducer for homepage & add 2 root reducer

---
+++ src/store/homePage/reducer.js
@@ -0,0 +1,9 @@
+const initialState = [];
+
+export default function HomePageSliceReducer(state = initialState, action) {
+  switch (action.type) {
+    default: {
+      return state;
+    }
+  }
+}
