dbfdf7382f5687a5e7707b94a545cce5fdbc2106
vishakahegde <hegde.vishaka@gmail.com>
2020-05-31 12:21:53 +0200

    create HomePages page and link use in App.js

---
+++ src/pages/HomePages/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Jumbotron } from "react-bootstrap";
+
+export default function HomePages() {
+  return (
+    <div>
+      <Jumbotron>
+        <h1>Homepages</h1>
+      </Jumbotron>
+    </div>
+  );
+}
