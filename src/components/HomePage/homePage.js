1f7da98cd8f6f72257e76aa3964298b7843c7573
vishakahegde <hegde.vishaka@gmail.com>
2020-05-31 18:33:04 +0200

    Create component with props to display list of homepages

---
+++ src/components/HomePage/homePage.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Jumbotron } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+export default function HomePage(props) {
+  return (
+    <Jumbotron
+      style={{
+        backgroundColor: props.backgroundColor,
+        color: props.color,
+      }}
+    >
+      <h2>{props.title}</h2>
+      <p>{props.description}</p>
+      <Link to={`/homepages/${props.id}`}>
+        <button>Visit Page</button>
+      </Link>
+    </Jumbotron>
+  );
+}
--- src/pages/HomePages/index.js
+++ src/pages/HomePages/index.js
@@ -1,12 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Jumbotron } from "react-bootstrap";
+import { apiUrl } from "../../config/constants";
+import axios from "axios";
+import HomePage from "../../components/HomePage/homePage";
 
 export default function HomePages() {
+  const [homePages, setHomePages] = useState([]);
+
+  async function getHomePages() {
+    const res = await axios.get(`${apiUrl}/homepages`);
+    console.log("Response:", res.data);
+    setHomePages(res.data);
+  }
+
+  useEffect(() => {
+    getHomePages();
+  }, []);
+
   return (
     <div>
       <Jumbotron>
         <h1>Homepages</h1>
       </Jumbotron>
+      {homePages.map((page) => {
+        return (
+          <HomePage
+            title={page.title}
+            key={page.id}
+            description={page.description}
+            id={page.id}
+            backgroundColor={page.backgroundColor}
+            color={page.color}
+          />
+        );
+      })}
     </div>
   );
 }
